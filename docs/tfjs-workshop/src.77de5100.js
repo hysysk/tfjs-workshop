fa16a45e87842452e7e7029fa182510d96167e8f
Yosuke Hayashi <hayashi.yosuke@gmail.com>
2019-04-06 15:56:31 +0900

    Fix mobilenet path

--- docs/tfjs-workshop/src.77de5100.js
+++ docs/tfjs-workshop/src.77de5100.js
@@ -19526,13 +19526,13 @@ Script.prototype.runInContext = function (context) {
     if (!(context instanceof Context)) {
         throw new TypeError("needs a 'context' argument.");
     }
-    
+
     var iframe = document.createElement('iframe');
     if (!iframe.style) iframe.style = {};
     iframe.style.display = 'none';
-    
+
     document.body.appendChild(iframe);
-    
+
     var win = iframe.contentWindow;
     var wEval = win.eval, wExecScript = win.execScript;
 
@@ -19541,7 +19541,7 @@ Script.prototype.runInContext = function (context) {
         wExecScript.call(win, 'null');
         wEval = win.eval;
     }
-    
+
     forEach(Object_keys(context), function (key) {
         win[key] = context[key];
     });
@@ -19550,11 +19550,11 @@ Script.prototype.runInContext = function (context) {
             win[key] = context[key];
         }
     });
-    
+
     var winKeys = Object_keys(win);
 
     var res = wEval.call(win, this.code);
-    
+
     forEach(Object_keys(win), function (key) {
         // Avoid copying circular objects like `top` and `window` by only
         // updating existing context properties or new properties in the `win`
@@ -19569,9 +19569,9 @@ Script.prototype.runInContext = function (context) {
             defineProp(context, key, win[key]);
         }
     });
-    
+
     document.body.removeChild(iframe);
-    
+
     return res;
 };
 
@@ -55731,7 +55731,7 @@ function () {
             _a = this;
             return [4
             /*yield*/
-            , tf.loadLayersModel('mymobilenet/model.json')];
+            , tf.loadLayersModel('tfjs-workshop/mymobilenet/model.json')];
 
           case 1:
             _a.model = _c.sent();
